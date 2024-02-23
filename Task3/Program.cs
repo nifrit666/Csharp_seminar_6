a381f2de41385f4aa8e68c6648d98bb168766b38
«Andrey_Istomin» <an.istomin@gmail.com>
2024-02-24 01:08:51 +0600

    Решение Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,56 @@
+﻿// void Main()
+// {
+//     string? inputString = System.Console.ReadLine();
+//     string findedLetters = "aeiouy";
+
+//     int count = 0;
+//     foreach(char ch in inputString)
+//     {
+//         if(findedLetters.Contains(ch))
+//             count ++;
+//     }
+
+//     System.Console.WriteLine($"{count} гласных букв");
+// }
+
+// Main();
+
+﻿/*
+Считать строку с консоли, состояющую из латинских букв в нижнем регистре.
+Выяснить, сколько среди введённых букв гласных.
+
+Пример:
+"hello" => 2
+"world" => 1
+*/
+
+//using System.Runtime.Intrinsics.Arm;
+
+
+
+int CountVowels(string str)
+{
+    string stringVowels = "aeiouy";
+    int count = 0;
+    str = str.ToLower();
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        for (int j = 0; j < stringVowels.Length; j++)
+        {
+            if (str[i] == stringVowels[j])
+            {
+                count++;
+            }
+
+        }
+    }
+
+    return count;
+}
+
+Console.WriteLine("Введите строку из латинских букв: ");
+string str = Console.ReadLine();
+
+int count = CountVowels(str);
+Console.WriteLine(count);
