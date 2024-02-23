dd144303999455c8d0fbde81ab7ac96111ab1673
«Andrey_Istomin» <an.istomin@gmail.com>
2024-02-24 00:54:25 +0600

    Решение Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿﻿/*
+Задайте массив символов (типа char[]).
+Создайте строку из символов этого массива.
+Пример: 
+['a', 'b', 'c', 'd'] => "abcd"
+*/
+
+char[] ch = { 'a', 'b', 'c', 'd' };
+
+string str = string.Empty; //пустая строка 
+
+for (int i = 0; i < ch.Length; i++)
+{
+    str += ch[i];
+}
+Console.WriteLine(str);
